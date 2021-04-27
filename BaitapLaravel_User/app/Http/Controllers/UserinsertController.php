e7a53fa12c1e4d3709bacdb7974b50fc310b32fc
Duong <thuy_duong@mut.co.jp>
2021-04-27 17:30:15 +0900

    test

---
+++ BaitapLaravel_User/app/Http/Controllers/UserinsertController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+use App\StudInsert;
+use Illuminate\Http\Request;
+use \Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class UserinsertController extends Controller
+{
+    public function index()
+    {
+        return view('user-list-view');
+    }
+}
+
+
