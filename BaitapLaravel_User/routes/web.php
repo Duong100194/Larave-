0a43effabef1afba22e5d5524c8b7800b74f83d3
Duong <thuy_duong@mut.co.jp>
2021-04-27 17:50:44 +0900

    edit web.php

--- BaitapLaravel_User/routes/web.php
+++ BaitapLaravel_User/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('user-list-view');
 });
+*/
+Route::get('/user-list-view', [UserinsertController::class, 'index'])->name('showlist');
